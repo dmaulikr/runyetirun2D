410973b6e9e69fe481c2b0988905915dff6517bc
Jorge Jordan <j.jordanarenas@gmail.com>
2014-04-22 20:00:18 +0200

    Initial Commit

---
+++ RunYetiRun/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  RunYetiRun
+//
+//  Created by Jorge Jordán Arenas on 22/04/14.
+//  Copyright Insane Platypus Games 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ RunYetiRun/Classes/HelloWorldScene.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldScene.h
+//  RunYetiRun
+//
+//  Created by Jorge Jordán Arenas on 22/04/14.
+//  Copyright Insane Platypus Games 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using Cocos2D v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The main scene
+ */
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (HelloWorldScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
