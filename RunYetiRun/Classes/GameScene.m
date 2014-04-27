55f4658e22b236c850c8d0e2846b053559d7870a
jjordanarenas <j.jordanarenas@gmail.com>
2014-04-27 22:23:41 +0200

    STEP 4 - Moving the yeti

--- RunYetiRun/Classes/GameScene.m
+++ RunYetiRun/Classes/GameScene.m
@@ -34,9 +34,30 @@
         CCSprite *background = [CCSprite spriteWithImageNamed:@"background.png"];
         background.position = CGPointMake(screenSize.width / 2, screenSize.height / 2);
         [self addChild:background z:-1];
+        
+        // STEP 4 - Moving the Yeti //https://www.makegameswith.us/gamernews/359/cocos2d-30-a-brief-transition-guide
+        // to enable touch handling on you CCNode:
+        self.userInteractionEnabled = TRUE;
+        
     }
     
     return self;
+}
+
+// STEP 4 - Moving the Yeti
+-(void) update:(CCTime)delta
+{//https://www.makegameswith.us/gamernews/359/cocos2d-30-a-brief-transition-guide
+    CCLOG(@"%@", @"Schedule update is called automatically");
+}
+
+// STEP 4 - Moving the Yeti
+// to catch a touch and its touch position:
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    CGPoint touchLocation = [touch locationInNode:self];
+    // Explain new way of execting actions
+    CCAction *actionMove = [CCActionMoveTo actionWithDuration:0.5 position:CGPointMake(yeti.position.x, touchLocation.y)];
+    [yeti runAction:[CCActionSequence actionWithArray:@[actionMove]]];
 }
 
 @end
